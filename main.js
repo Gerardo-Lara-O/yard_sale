6ddc4442b702200e151152bc8830ba81309a4040
Gerardo Lara <gerardo.lara_sic@tesco.edu.mx>
2025-02-13 22:06:53 -0600

    Lista de productos: HTML a partir de arrays

--- main.js
+++ main.js
@@ -25,3 +25,56 @@ function toggleProductDetail() {
     mobileMenu.classList.add('inactive')
     desktopMenu.classList.add('inactive')
 }
+
+const productList = [];
+
+    productList.push({
+        name: 'Bike',
+        price: 120,
+        image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+    })
+    productList.push({
+        name: 'Pantalla',
+        price: 120,
+        image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+    })
+    productList.push({
+        name: 'Bici',
+        price: 120,
+        image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+    })
+
+    const cardsContainer = document.querySelector('.cards-container')
+
+    function renderProducts(arr){
+        for (product of arr){
+            const productCard = document.createElement('div')
+            productCard.classList.add('product-card')
+            const image = document.createElement('img')
+            image.setAttribute('src', product.image)
+            image.setAttribute('alt',product.name)
+        
+            const productInfo = document.createElement('div')
+            productInfo.classList.add('product-info')
+            const productInfoDiv = document.createElement('div')
+            const productPrice = document.createElement('p')
+            productPrice.innerText = "$" + product.price
+            const productName = document.createElement('p')
+            productName.innerText = product.name
+        
+            productInfoFigure = document.createElement('figure')
+            productImgCart = document.createElement('img')
+            productImgCart.setAttribute('src', './icons/bt_add_to_cart.svg')
+        
+        
+            productInfoFigure.appendChild(productImgCart)
+            productInfoDiv.append(productPrice,productName)
+            productInfo.append(productInfoDiv,productInfoFigure)
+            productCard.append(image,productInfo)
+        
+            cardsContainer.appendChild(productCard)
+        }
+    }
+
+    renderProducts(productList)
+
