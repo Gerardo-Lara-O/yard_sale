71d1f273d15b2b074ef4c4b805ecdd402e0c5602
Gerardo Lara <gerardo.lara_sic@tesco.edu.mx>
2025-02-13 20:02:25 -0600

    Agregando carrito de compras

--- main.js
+++ main.js
@@ -4,12 +4,24 @@ const desktopMenu = document.querySelector('.desktop-menu')
 const menuHamIcon = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu')
 
+const navbarShoppingCart = document.querySelector('.navbar-shopping-cart')
+const aside = document.querySelector('.product-detail')
+
 navbarEmail.addEventListener('click', toggleDesktopMenu)
 menuHamIcon.addEventListener('click', toggleMobileMenu)
+navbarShoppingCart.addEventListener('click', toggleProductDetail)
 
-function toggleDesktopMenu(){
+
+function toggleDesktopMenu() {
     desktopMenu.classList.toggle('inactive')
+    aside.classList.add('inactive')
 }
-function toggleMobileMenu(){
+function toggleMobileMenu() {
     mobileMenu.classList.toggle('inactive')
+    aside.classList.add('inactive')
+}
+function toggleProductDetail() {
+    aside.classList.toggle('inactive')
+    mobileMenu.classList.add('inactive')
+    desktopMenu.classList.add('inactive')
 }
